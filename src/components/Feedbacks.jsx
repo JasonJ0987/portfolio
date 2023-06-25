120193338f0b55a9afdd86ec3607fe21e0ec5918
Jason <chiyoung.jang@mail.mcgill.ca>
2023-06-25 11:33:37 -0400

    added stars and earth models

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -5,7 +5,7 @@ import { fadeIn, textVariant } from "../utils/motion";
 import { testimonials } from "../constants";
 
 const FeedbackCard = ({ index, testimonial, name, designation, company, image}) => (
-  <motion.div variants={fadeIn("", "spring", index*0.5, 0.75)} className="bg-black-200 p-10 rounded-3xl xs: w-[320px] w-full">
+  <motion.div variants={fadeIn("", "spring", index*0.5, 0.75)} className="bg-black-200 p-10 rounded-3xl xs:w-[320px] w-full">
     <p className="text-white font-black text-[48px]">"</p>
 
     <div className="mt-1">
